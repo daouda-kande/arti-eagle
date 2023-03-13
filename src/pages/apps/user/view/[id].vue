48ffefb0b050b4b6b45256c7a0b39c25c402bfef
Gren CISSE Al-Karid <grencisse@gmail.com>
2023-03-13 12:46:32 +0000

    Fix tab overview display issue

--- src/pages/apps/user/view/[id].vue
+++ src/pages/apps/user/view/[id].vue
@@ -6,7 +6,7 @@ import UserTabAbsence from '@/views/apps/user/view/UserTabAbsence.vue'
 import UserTabBillingsPlans from '@/views/apps/user/view/UserTabBillingsPlans.vue'
 import UserTabConnections from '@/views/apps/user/view/UserTabConnections.vue'
 import UserTabNotifications from '@/views/apps/user/view/UserTabNotifications.vue'
-import UserTabProject from '@/views/apps/user/view/UserTabProject.vue'
+import UserTabOverview from '@/views/apps/user/view/UserTabOverview.vue'
 import UserTabSecurity from '@/views/apps/user/view/UserTabSecurity.vue'
 
 
@@ -23,14 +23,6 @@ const tabs = [
   {
     icon: 'tabler-user-check',
     title: 'Projets',
-  },
-  {
-    icon: 'tabler-lock',
-    title: 'Evaluations',
-  },
-  {
-    icon: 'tabler-currency-dollar',
-    title: 'Formations',
   },
 ]
 
@@ -83,7 +75,7 @@ userListStore.fetchUser(Number(route.params.id)).then(response => {
         </VWindowItem>
         
         <VWindowItem>
-          <UserTabProject />
+          <UserTabOverview />
         </VWindowItem>
 
         <VWindowItem>

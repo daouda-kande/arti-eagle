f6ddf6f8b3b8c6e06f9298c5dbcb6d1b36f174aa
Gren CISSE Al-Karid <grencisse@hotmail.com>
2023-04-18 16:45:24 +0000

    Add date picker in vertical leyout navbar

---
+++ src/layouts/components/CalendarShortcut.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue';
+
+</script>
+
+<template>
+  <AppDateTimePicker 
+    :model-value="new Date().toJSON().slice(0, 10)"
+    label="Choisir une date"
+    :config="{ altInput: true, altFormat: 'F j, Y', dateFormat: 'Y-m-d', altLocal: 'fr-FR' }"
+  />
+</template>

bdf56a4df532eee7fefbbd70c0de86b332a31f2a
daouda-kande <daoudakand72@gmail.com>
2023-04-19 16:43:40 +0000

    Update Configs

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://172.28.22.131:8000/api/v1"
+/* export const API_BASE_URL = "http://172.28.22.131:8000/api/v1" */
 
-//export const API_BASE_URL = "http://192.168.0.218:8000/api/v1"
+export const API_BASE_URL = "http://192.168.0.218:8000/api/v1"

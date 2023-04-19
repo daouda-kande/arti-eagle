02ad650488d830a62390df3b8ffd92eca5f69d15
Gren CISSE Al-Karid <grencisse@hotmail.com>
2023-04-19 11:29:10 +0000

    Update API_BASE_URL

--- src/views/dashboards/checkins/useCheckinStore.js
+++ src/views/dashboards/checkins/useCheckinStore.js
@@ -1,9 +1,10 @@
 import axios from '@axios'
+import { API_BASE_URL } from '@projectConfig'
 import { defineStore } from 'pinia'
 
 export const useCheckinStore = defineStore('CheckinStore', {
   actions: {
     // 👉 Fetch users data
-    fetchCheckin() { return axios.get('http://172.28.22.131:8000/api/v1/dashboard/checkins/') },
+    fetchCheckin() { return axios.get(`${API_BASE_URL}/dashboard/checkins/`) },
   },
 })

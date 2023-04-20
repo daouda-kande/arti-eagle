002484d66df30a10833d39269d1640a570d99435
Gren CISSE Al-Karid <grencisse@hotmail.com>
2023-04-20 08:54:06 +0000

    Add click event to calendar

--- src/layouts/components/CalendarShortcut.vue
+++ src/layouts/components/CalendarShortcut.vue
@@ -1,6 +1,12 @@
 <script setup>
 import AppDateTimePicker from '@core/components/AppDateTimePicker.vue';
 
+function onButtonClick(e) {
+  // Emit custom event with event name and optional data
+  console.log("DEBUG - EVENT")
+  console.log(e)
+  $emit("selectedDate",e)
+}
 </script>
 
 <template>
@@ -8,5 +14,6 @@ import AppDateTimePicker from '@core/components/AppDateTimePicker.vue';
     :model-value="new Date().toJSON().slice(0, 10)"
     label="Choisir une date"
     :config="{ altInput: true, altFormat: 'F j, Y', dateFormat: 'Y-m-d', altLocal: 'fr-FR' }"
+    @input="onButtonClick($event.target.value)"
   />
 </template>

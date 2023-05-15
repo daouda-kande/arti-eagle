8d8100e68eacab9f499f40f1b39c24e389e7d073
Al-Karid <grencisse@hotmail.com>
2023-05-15 11:18:37 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // export const API_BASE_URL = "http://172.28.22.131:8000/api/v1" // Soraya
 // export const API_BASE_URL = "http://172.18.221.242:8000/api/v1" // ARTI LAPTOP
-export const API_BASE_URL = "http://api:8000/api/v1" // ARTI LAPTOP
+export const API_BASE_URL = "http://192.168.0.175:8000/api/v1" // ARTI LAPTOP
 
 // export const API_BASE_URL = "http://192.168.0.218:8000/api/v1" //# ARTI SERVER PC
 //export const API_BASE_URL = "http://172.30.1.152:8000/api/v1" //# ARTI LAPTOP WSL
